e789983eeab409d416b0854a5188e48e86f6df03
andrewwillette <willette.andrew1846@gmail.com>
2024-04-21 20:06:33 -0500

    master: adding luadoc for types

--- lua/open/init.lua
+++ lua/open/init.lua
@@ -1,5 +1,11 @@
 local open = {}
 
+---@class OpenSelection
+---@field name string
+---@field url string
+
+---@param keymap string
+---@param to_open OpenSelection[]
 function open.set_keymap(keymap, to_open)
   local select_options = {}
   for _, v in pairs(to_open) do
